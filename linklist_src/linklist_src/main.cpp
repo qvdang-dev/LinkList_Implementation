48554afe6f9714b687563b7e1292ee4746dead96
qvdang96 <qvdang.96@gmail.com>
2020-04-18 18:32:02 +0700

    Mofidy code: Add "rear" node into linklist

--- linklist_src/linklist_src/main.cpp
+++ linklist_src/linklist_src/main.cpp
@@ -7,41 +7,60 @@ class node
 public:
 	int data;
 	node* next;
+	bool m_isHead;
+	bool m_isRear;
 };
 
 void insert(int value);
 void display();
 
 node* g_pnHead;
+node* g_pnRear;
+
 void insert(int value)
 {
 	node* pnTemp = new node();
 	pnTemp->data = value;
 	pnTemp->next = NULL;
+	pnTemp->m_isHead = false;
+	pnTemp->m_isRear = true;
 
-	if (g_pnHead != NULL)
-		pnTemp->next = g_pnHead;
+	if (g_pnRear == NULL)
+	{
+		g_pnHead = pnTemp;
+		g_pnHead->m_isHead = true;	
+	}
+	else
+	{
+		g_pnRear->next = pnTemp;
+		g_pnRear->m_isRear = false;
+	}
 
-	g_pnHead = pnTemp;
-
+	g_pnRear = pnTemp;
+	
 }
 
 void display()
 {
 	node* pnTemp = g_pnHead;
-	cout << "current link list: ";
+	cout << "Current LinkList: "<<endl;
 	while (pnTemp != NULL)
 	{
-		cout << "[" << pnTemp->data << "|" << pnTemp->next << "]" << "-->";
+		cout << "[Data: " << pnTemp->data;
+		cout << "|Addr: " << pnTemp;
+		cout << "|Next: " << pnTemp->next;
+		cout << "|IsHead: " << pnTemp->m_isHead << "|IsRear: ";
+		cout << pnTemp->m_isRear << "]\n";
 		pnTemp = pnTemp->next;
 	}
-	cout << " |NULL|\n";
+	cout << "|End|\n";
 }
 
 int main()
 {
 	int data, numOfNote;
 	g_pnHead = NULL;
+	g_pnRear = NULL;
 
 	cout << "Number of Node: ";
 	scanf_s("%d", &numOfNote);
@@ -50,7 +69,6 @@ int main()
 	{
 		cout << " + Data " << i << ": ";
 		scanf_s("%d", &data);
-		cout << "\n";
 		insert(data);
 	}
 	display();
